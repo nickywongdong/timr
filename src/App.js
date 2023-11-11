0e65e4a2a9842af00b086bf2b5aa3f9e3c0b3f39
Nick Wong (from Dev Box) <nickwong@microsoft.com>
2023-11-11 08:58:39 -0800

    Removing expo, adding react base app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-
-      <a href="https://github.com/nickywongdong/timr/actions/workflows/timr-ci.yml">
-        <img
-          src="https://github.com/nickywongdong/timr/actions/workflows/timr-ci.yml/badge.svg"
-          alt="timr-ci"
-        />
-      </a>
-      <StatusBar style="auto" />
-    </View>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Timr.
+        </p>
+        <a href="https://github.com/nickywongdong/timr/actions/workflows/timr-ci.yml">
+          <img
+            src="https://github.com/nickywongdong/timr/actions/workflows/timr-ci.yml/badge.svg"
+            alt="timr-ci"
+          />
+        </a>
+      </header>
+    </div>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
