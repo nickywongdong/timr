003888cf4010e1c506dd3bc4515c651ca224aace
Nick Wong (from Dev Box) <nickwong@microsoft.com>
2023-11-11 09:09:07 -0800

    Fixing test case

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Timr.
+          Welcome to timr.
         </p>
         <a href="https://github.com/nickywongdong/timr/actions/workflows/timr-ci.yml">
           <img
