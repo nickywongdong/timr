9d7f358e2d8efd00ff6d58a2ee49f471bc5570e3
nickywongdong <wongnich@gmail.com>
2023-11-14 21:08:46 -0800

    Add configuration page, and navbar to get there

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
+import React, { useState, useLayoutEffect } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './AppRoutes';
+import withSplashScreen from './Components/SplashScreen';
+import NavBar from './Components/NavBar';
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import withSplashScreen from './Components/SplashScreen';
-import Stopwatch from './Components/Stopwatch';
 
 function App() {
   const [isFading, setIsFading] = useState(false);
 
-  useEffect(() => {
-
+  useLayoutEffect(() => {
     const headerElement = document.querySelector('.welcome');
     headerElement.addEventListener('transitionend', handleTransitionEnd);
 
-    // Set a timeout to trigger the fade-out effect after 1 seconds
     const fadeTimeout = setTimeout(() => {
       setIsFading(true);
-    }, 1000); // 1 seconds
+    }, 1000);
 
     return () => {
       clearTimeout(fadeTimeout);
       headerElement.removeEventListener('transitionend', handleTransitionEnd);
-    }
+    };
   }, []); // Empty dependency array ensures this runs only once on component mount
-
 
   const handleTransitionEnd = () => {
     const headerElement = document.querySelector('.welcome');
@@ -34,25 +33,28 @@ function App() {
   const fadeOutClass = isFading ? 'fade-out' : '';
 
   return (
-    <div className="AppContainer">
-      <header className={`welcome ${fadeOutClass}`}>
-        <img src={logo} className="AppLogo" alt="React Logo" />
-        <p>
-          Welcome to timr.
-        </p>
-      </header>
-      <div className="stopwatchContainer">
-        <Stopwatch />
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="AppContainer">
+          <header className={`welcome ${fadeOutClass}`}>
+            <img src={logo} className="AppLogo" alt="Timr Logo" />
+            <p>Welcome to timr.</p>
+          </header>
+          <div className="stopwatchContainer"><AppRoutes /></div>
+        </div>
+        <footer>
+            <div>
+              <a href="https://github.com/nickywongdong/timr/actions/workflows/timr-ci.yml">
+                <img
+                  src="https://github.com/nickywongdong/timr/actions/workflows/timr-ci.yml/badge.svg"
+                  alt="Timr Continuous Integration Badge"
+                />
+              </a>
+            </div>
+          </footer>
       </div>
-      <footer>
-        <a href="https://github.com/nickywongdong/timr/actions/workflows/timr-ci.yml">
-          <img
-            src="https://github.com/nickywongdong/timr/actions/workflows/timr-ci.yml/badge.svg"
-            alt="Timr Continuous Integration Badge"
-          />
-        </a>
-      </footer>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/Components/ConfigurationPage/index.jsx
@@ -0,0 +1,13 @@
+// ConfigurationPage.js
+import React from 'react';
+
+function ConfigurationPage() {
+  return (
+    <div>
+      {/* Content for your configuration page */}
+      <h1>Configuration Page</h1>
+    </div>
+  );
+}
+
+export default ConfigurationPage;
