f76c7ad07df79e4703f9990e290f8d2e825cd2fe
nickywongdong <wongnich@gmail.com>
2024-02-06 22:51:48 -0800

    Removing sample feature, adding appbar with settings button

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,8 +3,6 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:timr/src/features/timepicker.dart';
 
-import 'sample_feature/sample_item_details_view.dart';
-import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
 
@@ -70,8 +68,6 @@ class MyApp extends StatelessWidget {
                 switch (routeSettings.name) {
                   case SettingsView.routeName:
                     return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
                   case TimerPickerExample.routeName:
                   default:
                     return const TimerPickerExample();
--- lib/src/features/timepicker.dart
+++ lib/src/features/timepicker.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+
+import '../settings/settings_view.dart';
 
 class TimerPickerExample extends StatefulWidget {
   const TimerPickerExample({super.key});
@@ -15,11 +18,23 @@ class _TimerPickerExampleState extends State<TimerPickerExample> {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        middle: Text('timr'),
+    return Scaffold(
+
+      appBar: AppBar(
+        title: const Text('timr'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              // Navigate to the settings page. If the user leaves and returns
+              // to the app after it has been killed while running in the
+              // background, the navigation stack is restored.
+              Navigator.restorablePushNamed(context, SettingsView.routeName);
+            },
+          ),
+        ],
       ),
-      child: DefaultTextStyle(
+      body: DefaultTextStyle(
         style: TextStyle(
           color: CupertinoColors.label.resolveFrom(context),
           fontSize: 22.0,
